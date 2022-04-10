b0505197f9d8c8005e8c0ec3279e0c25a48cd59e
Greg Bouwens <greg.bouwens@hanselware.com>
2022-04-10 20:26:03 +0000

    Authenticating requests to Spring Data REST Api with HTTP Basic

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,13 +2,24 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication()
 public class ResolutionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionsApplication.class, args);
+    }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                org.springframework.security.core.userdetails.User
+                        .withUsername("user")
+                        .password("{bcrypt}$2b$10$0BCOSk2WSnQPA5VPv7qDw.P5jx1iW47xzhbe.32ryXGdLJ6QlxDZy")
+                        .authorities("resolution:read")
+                        .build());
+    }
 }
